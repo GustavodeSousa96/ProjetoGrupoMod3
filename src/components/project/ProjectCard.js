594e3bc1185f106de74ba46b0a1a9dda68109f0c
unknown <joseg081@gmail.com>
2023-01-08 19:54:25 -0300

    button nos cards

--- src/components/project/ProjectCard.js
+++ src/components/project/ProjectCard.js
@@ -1,32 +1,33 @@
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import styles from './ProjectCard.module.css'
-import {BsPencil, BsFillTrashFill} from 'react-icons/bs' //Assim se puxam os ícones
+import { BsPencil, BsFillTrashFill } from 'react-icons/bs' //Assim se puxam os ícones
+import { IoMdPhotos } from 'react-icons/io';
 
 function ProjectCard({id, name, rgi, cidade, preco, estado, handleRemove}){
-
     const remove = (e) => {
-        e.preventDefault()
-        handleRemove(id)
-        
+      e.preventDefault()
+      handleRemove(id)
     }
 
-    return(
-       <div className={styles.project_card}>
+    return (
+        <div className={styles.project_card}>
          <h4>{name}</h4>
-         <p className={styles.category_text}><strong>RGI:</strong> {rgi} </p>
-         <p className={styles.category_text}><strong>Cidade:</strong> {cidade} <strong> UF:</strong> {estado}</p>
-         <p className={styles.category_text}><strong>Preço:</strong> R${preco},00 </p>
+         <p className={styles.category_text}><strong>RGI: &nbsp;</strong> {rgi} </p>
+         <p className={styles.category_text}><strong>Cidade: &nbsp;</strong> {cidade} <strong> &nbsp;UF:&nbsp;</strong> {estado}</p>
+         <p className={styles.category_text}><strong>Preço: &nbsp;</strong> R${preco},00 </p>
          <div className={styles.project_card_actions}>
             <Link to={`/imovel/${id}`}>
                 <BsPencil /> Editar
             </Link>
+            <button>
+                <IoMdPhotos /> Ver
+            </button>
             <button onClick={remove}>
                 <BsFillTrashFill /> Excluir
             </button>
             
          </div>
        </div>
-        
     )
 }
 
