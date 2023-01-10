3b55c3d15284bac06ec2b5f5e8ccc9fa6238be0b
Patryck Campos <114076569+PatryckCampos@users.noreply.github.com>
2023-01-10 18:13:13 -0300

    Update Card.js
    Card modificado para o home

--- src/components/layout/Card.js
+++ src/components/layout/Card.js
@@ -1,18 +1,24 @@
 import React from "react";
 import styles from './Card.module.css';
+import LinkButton from "./LinkButton";
+import { FaSink } from "react-icons/fa";
+import { BiBed } from "react-icons/bi";
 
-function Card({titulo, conteudo, Imagem}) {
+function Card({titulo, localizacao, valor, Imagem, caminho, texto}) {
     return(
     
         <div className={styles.card}>
             
             <div className={styles.card_body}>
-            <img src={Imagem} alt="imagem ilustrativa"/>
+            <img src={Imagem} className={styles.img} style={{width:'100%'}} alt="imagem ilustrativa"/>
             
             <h3>{titulo}
             </h3>
-
-            <p>{conteudo}</p>
+            <p><strong>Estande de venda:</strong> {localizacao}</p>
+            <p><strong>Valor:</strong> R$ {valor},00</p>
+            <p> <FaSink/> 1 banheiro  </p>
+            <p> <BiBed/> 2 quarto </p>
+            <LinkButton to={caminho} text={texto} style={{margin:'bottom'}}/>
             </div>
         </div>
 
