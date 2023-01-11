5ff3425d757de60aafee4aa9a819e1018fd05c58
unknown <joseg081@gmail.com>
2023-01-10 22:24:11 -0300

    box shadow e afins

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 import Container from './Container';
 import styles from './Navbar.module.css';
-import logo from '../../img/costs_logo.png'; // Alterar a logo do Navbar aqui
+import logo from '../../img/imoveis/favicon.png'; // Alterar a logo do Navbar aqui
 
 function Navbar() {
   return (
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,7 +9,7 @@ import Loading from '../../components/layout/Loading';
 import styles from './Home.module.css';
 
 //importação da imagem
-import Rio from '../../img/costs_logo.png';
+import Rio from '../../img/imoveis/favicon.png';
 import LinkButton from '../layout/LinkButton';
 
 //importação do card
@@ -17,6 +17,9 @@ import Card from '../layout/Card';
 import Sobrado from '../../img/imoveis/casa_1_1.jpg';
 import Sobrado1 from '../../img/imoveis/casa_2.jpg';
 import Sobrado2 from '../../img/imoveis/casa_3.jpg'; 
+
+// importacao do icone
+import { MdAttachMoney } from "react-icons/md";
 
 function Home() {
   const [isLoading, setIsLoading] = useState(false);
@@ -35,11 +38,14 @@ function Home() {
       ) : (
         <>
           <h1 className={styles.h1}>
-            <span className={styles.span}>Bem-vindo ao Rio Imóveis</span>
+            <span className={styles.span}>Bem-vindo à Rio Imóveis</span>
           </h1>
           <div className={styles.grid}>
             <div className={styles.conteudo}> 
-          <p className={styles.texto} style={{color:'white'}}> <img src={Rio} alt='Rio Imoveis'/>
+          <p className={styles.texto} style={{color:'white'}}> 
+          <div className={styles.textImg}>
+            <img src={Rio} alt='Rio Imoveis'/>
+          </div>
           &nbsp; &nbsp;A Rio Imóveis é uma empresa voltada para comercialização e locação de imóveis, também focada em diversos negócios do mercado imobiliário. Nos especializamos na locação de imóveis mobiliados, aluguel ou por temporada e venda, em diversos bairros do Rio de janeiro.
           <br/>
           <br/>
@@ -53,7 +59,6 @@ function Home() {
             </p>
             </div>
           </div>
-          <p className='title-cards'>Gerenciar imóveis cadastrados</p>
             <div className={styles.linha}>
               <Card Imagem={Sobrado} titulo='Sobrado Para Alugar' localizacao='Rua Prudente de Morais - Ipanema, Rio de Janeiro, RJ' valor='1100' caminho="/dashimoveis" texto="Alugue"/>
               <Card Imagem={Sobrado1} titulo='Sobrado Para Venda' localizacao='Rua Joaquim Nabuco - Ipanema, Rio de Janeiro, RJ' valor='1599' caminho="/dashimoveis" texto="Compre"/>
@@ -61,7 +66,12 @@ function Home() {
             </div>
             <br/>
             <br/>
-            <LinkButton to="/imoveis" text="Cadastrar imóveis" />
+
+            <div className={styles.homeFooter}>
+              <h1 className={styles.titleVenda}> <MdAttachMoney/> Deseja vender seu Imóvel?  <MdAttachMoney/> </h1>
+              <p>Apenas com um clique, você é capaz de anunciar seu Imóvel na maior imobiliária	do Brasil!!! </p>
+              <LinkButton to="/imoveis" text="Cadastrar imóveis" />
+            </div>
         </>
       )}
     </div>
